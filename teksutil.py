f3b35a6aa0f732c6d1ac07b1afa2174e1c28df6c
trainerpemrograman <trainer_pemrograman@gamelab.id>
2023-12-05 14:40:05 +0700

    update aplikasi

--- teksutil.py
+++ teksutil.py
@@ -1,4 +1,8 @@
-def hitung_kata(teks):
-    words = teks.split()
-    words = [word for word in words if word.lower() != "the"]
-    return len(words)
+def menghitung_kata():
+    masukan_kata_kamu = input('Masukan Sebuah Kata: ')
+    jadikan_list = masukan_kata_kamu.split()
+    hitung_jumlah_kata = len(jadikan_list)
+    print ('Jumlah Kalimat:', hitung_jumlah_kata)
+    print ('Jumlah huruf:', len(masukan_kata_kamu))
+
+menghitung_kata()
